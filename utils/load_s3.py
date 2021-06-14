85449733229509b60ec7276726214f104e47765b
Hartvig Johannson <hartvig.johannson@fortumcos.com>
2021-06-14 18:02:34 +0200

    download from s3

--- utils/load_s3.py
+++ utils/load_s3.py
@@ -7,8 +7,8 @@ def download_and_extract_images():
 
     # enter authentication credentials
     s3 = boto3.resource('s3',
-                        aws_access_key_id='AKIATIA7L73JHFAFYGF3',
-                        aws_secret_access_key='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')
+                        aws_access_key_id='',
+                        aws_secret_access_key='')
 
     s3.Bucket(BUCKET_NAME).download_file('imdbFaces.zip', 'imdbFaces.zip')
     s3.Bucket(BUCKET_NAME).download_file('imdb.csv', 'imdb.csv')
