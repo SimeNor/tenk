708425ff9c725fecf56341d9d1437934116357dd
Simen Norrheim Larsen <simen-norrheim.larsen@capgemini.com>
2021-07-01 15:04:57 +0200

    Cropped about 50k images and tested some model testing

--- utils/util.py
+++ utils/util.py
@@ -3,7 +3,7 @@ import numpy as np
 from mtcnn.mtcnn import MTCNN
 import os
 import datetime as dt
-
+import time
 
 def matlab2datetime(matlab_datenum):
     day = dt.datetime.fromordinal(int(matlab_datenum))
@@ -59,3 +59,50 @@ def extract_faces(path_images:str, path_cropped_images:str=None, output_image_si
         if path_cropped_images is not None:
             img = Image.fromarray(face)
             img.save(os.path.join(path_cropped_images, file_name))
+
+def extract_faces_paths(root_path:str, original_paths:list, path_cropped_images:str=None, output_image_size:tuple=None) -> dict:
+    # Confirm path to store images to
+    os.makedirs(path_cropped_images, exist_ok=True)
+
+    counter = 0
+    total = len(original_paths)
+    logged_times = []
+    
+    # Extract faces
+    for file_name in original_paths:
+        
+        start = time.time()
+        file_path = os.path.join(root_path, file_name)
+
+        new_path = os.path.join(path_cropped_images, file_name)
+        
+        counter += 1
+
+        if os.path.exists(new_path):
+            continue
+
+        # Extract face
+        try:
+            face = extract_face(file_path, output_image_size)
+        except:
+            continue
+        
+        try:
+            # Save image to folder
+            if path_cropped_images is not None:
+                
+                os.makedirs(new_path[:-len(new_path.split("/")[-1])], exist_ok=True)
+
+                img = Image.fromarray(face)
+                img.save(new_path)
+
+                # if counter % 10 == 9:
+                elapsed_time = time.time() - start
+                logged_times.append(elapsed_time)
+                
+                if len(logged_times) > 50:
+                    logged_times = logged_times[-50:]
+                
+                    print(f"{counter}/{total} - ETA: {time.strftime('%H:%M:%S', time.gmtime(np.mean(logged_times) * (total - counter)))}")
+        except:
+            continue
