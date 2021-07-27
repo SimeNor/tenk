f7e731a82c899e072ad4d16eaa0a8b7d6cc5541d
Simen Norrheim Larsen <simen-norrheim.larsen@capgemini.com>
2021-07-27 16:32:02 +0200

    changed setup

--- utils/init_setup.py
+++ utils/init_setup.py
@@ -1,11 +1,12 @@
 import os, warnings, json
 
-warnings.filterwarnings('ignore')
-os.chdir("tenk")
-os.system('pip install -r utils/requirements.txt')
-os.system('unzip cropped.zip && rm uncropped.zip')
+def setup():
+    warnings.filterwarnings('ignore')
+    os.chdir("tenk")
+    os.system('pip install -r utils/requirements.txt')
+    os.system('unzip cropped.zip && rm uncropped.zip')
 
-temp = {'workers': 0 if os.name == 'nt' else 2}
+    temp = {'workers': 0 if os.name == 'nt' else 2}
 
-with open('_temp_.json', 'w') as f:
-    json.dump(temp, f)
+    with open('_temp_.json', 'w') as f:
+        json.dump(temp, f)
