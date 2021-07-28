798f335b65f3889724d5d3cbf89af95f531263f1
Simen Norrheim Larsen <simen-norrheim.larsen@capgemini.com>
2021-07-28 14:05:41 +0200

    Altered file names for increased understanding

--- utils/init_setup.py
+++ utils/init_setup.py
@@ -4,7 +4,7 @@ def setup():
     warnings.filterwarnings('ignore')
     os.chdir("tenk")
     os.system('pip install -r utils/requirements.txt')
-    os.system('unzip cropped.zip bilder_kjendiser')
+    os.system('unzip komp_kjendiser.zip')
 
     temp = {'workers': 0 if os.name == 'nt' else 2}
 
